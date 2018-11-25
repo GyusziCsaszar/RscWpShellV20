5891c875f46a7ee5966e7b1f22bc8080fd0ce94c
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2018-11-25 16:24:10 +0100

    Initial commit.

---
+++ RscWpShellV20_Camera/RscCamera/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RscCamera.Resources;
+
+namespace RscCamera
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
