5022e4113a63249660deb25f54ee81714a82d8b4
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2018-11-25 15:56:12 +0100

    Initial commit.

---
+++ RscWpShellV20/Launcher_Agent/TaskProgressTileUpdater.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Shell;
+using System.Linq; 
+
+using Launcher_Lib;
+
+namespace Launcher_Agent
+{
+
+    public class TaskProgressTileUpdater : IBackgroundTaskHelper 
+    {
+
+        public event EventHandler<TaskHelperEventArgs> Completed;
+
+        public void Start()
+        {
+            DoWork();   
+        }
+
+        public void DoWork()
+        {
+
+            try
+            {
+				// //
+				//
+				
+				System.Threading.ManualResetEvent mre = new System.Threading.ManualResetEvent(false);
+				
+				Deployment.Current.Dispatcher.BeginInvoke(() =>
+            	{
+					try
+					{
+						SysTiles st = new SysTiles();
+						st.DoUpdate( true, null );
+					}
+					catch( Exception )
+					{
+						//NOP...
+					}
+					
+					mre.Set();
+				});
+
+				// Wait for Lock Screen image to complete
+				mre.WaitOne();
+				// Then reset for the Tile Image operation
+				mre.Reset();
+				
+				//
+				// //
+				
+                Status = TaskHelperCompletionStatus.Completed;
+                
+            }
+            catch (Exception)
+            {
+                Status = TaskHelperCompletionStatus.Failed; 
+            }
+            NotifyComplete();
+        }
+
+        private void NotifyComplete()
+        {
+            EventHandler<TaskHelperEventArgs> eh = Completed;
+            if (eh != null)
+                eh(this, new TaskHelperEventArgs(Status));
+        }
+
+        public void Stop()
+        {
+            Status = TaskHelperCompletionStatus.TimedOut;
+            NotifyComplete();
+        }
+
+        public int ProgressCompleted
+        {
+            get;
+            private set;
+        }
+
+        public TaskHelperCompletionStatus Status
+        {
+            get;
+            private set;
+        }
+
+    }
+}
---
+++ RscWpShellV20/Launcher_Agent/TaskScheduler.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Phone.Scheduler;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Device.Location;
+using Microsoft.Phone.Shell;
+using System;
+using System.Threading;
+
+namespace Launcher_Agent
+{
+	
+    public class TaskScheduler : ScheduledTaskAgent
+    {
+		
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            DoTileUpdates(null);    
+
+            TimeSpan waitToSyncThreads = TimeSpan.FromSeconds(8);
+            if (System.Diagnostics.Debugger.IsAttached)
+                waitToSyncThreads = TimeSpan.FromSeconds(30);
+
+            if (locationStatus == TaskHelperCompletionStatus.Blocked &&
+                updaterStatus == TaskHelperCompletionStatus.Blocked)
+                this.Abort();
+            else
+                this.NotifyComplete();
+        }
+
+        void DoTileUpdates(object ununsed)
+        {
+
+            TaskProgressTileUpdater updater = new TaskProgressTileUpdater();             
+            updater.Completed += new EventHandler<TaskHelperEventArgs>(updater_Completed);            
+            updater.Start();
+        }
+
+        WaitHandle[] runningHelperHandles;
+        IBackgroundTaskHelper[] runningHelpers;
+
+        TaskHelperCompletionStatus locationStatus = TaskHelperCompletionStatus.Started;
+        TaskHelperCompletionStatus updaterStatus = TaskHelperCompletionStatus.Started;
+        void updater_Completed(object sender, TaskHelperEventArgs e)
+        {
+            // throw new NotImplementedException();
+            updaterStatus = e.Status;
+            System.Diagnostics.Debug.WriteLine("Updater completed " + e.Status.ToString() + " " + DateTime.Now.ToString());
+
+        }
+
+        void locationService_Completed(object sender, TaskHelperEventArgs e)
+        {
+            locationStatus = e.Status;
+            if (runningHelperHandles.Length > 0 && runningHelperHandles[(int)TaskOrder.Location] != null)
+            {
+                AutoResetEvent evt = runningHelperHandles[(int)TaskOrder.Location] as AutoResetEvent;
+                if (evt != null)
+                    evt.Set();
+            }
+            System.Diagnostics.Debug.WriteLine("Location completed " + e.Status.ToString() + " " + DateTime.Now.ToString());
+        }
+
+        enum TaskOrder : int
+        {
+            Location,
+            TileUpdates,
+            End = 1
+        };
+		
+    }
+	
+}
