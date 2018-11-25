206ad98f09aa275494e9ad6b04b28790d5beff72
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2018-11-25 16:00:01 +0100

    Initial commit.

---
+++ RscWpShellV20/Launcher_AppMini/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Launcher_AppMini.Resources;
+
+namespace Launcher_AppMini
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
